76db10e778953927835806a2db37fc211ea2a8cd
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2022-08-28 18:57:11 +0300

    Add plugin nvim-tree

--- chadrc.lua
+++ chadrc.lua
@@ -9,7 +9,11 @@ M.plugins = {
   user = {
     ["goolord/alpha-nvim"] = require("custom.plugins.alpha-nvim"),
     ["folke/which-key.nvim"] = require("custom.plugins.which-key"),
-  }
+  },
+
+  override = {
+    ["kyazdani42/nvim-tree.lua"] = require("custom.plugins.nvim-tree"),
+  },
 }
 
 return M
