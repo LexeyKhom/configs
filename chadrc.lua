581509b80af98991761ed75a133ec8a185fe405b
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2022-09-15 13:17:39 +0300

    style(chadrc): Set chadracula theme

--- chadrc.lua
+++ chadrc.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.ui = {
-  theme_toggle = { "onedark", "one_light" },
-  theme = "onedark",
+  theme_toggle = { "chadracula", "one_light" },
+  theme = "chadracula",
 }
 
 M.plugins = {
