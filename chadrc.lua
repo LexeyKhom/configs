9201f95a768c78fe2e84c1f6bcca4aca28e2986a
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2022-09-20 10:46:28 +0300

    style(chadrc): change user and override places

--- chadrc.lua
+++ chadrc.lua
@@ -6,17 +6,18 @@ M.ui = {
 }
 
 M.plugins = {
+  -- Override
+  ["williamboman/mason.nvim"] = require "custom.plugins.mason",
+  ["kyazdani42/nvim-tree.lua"] = require "custom.plugins.nvim-tree",
+  ["nvim-treesitter/nvim-treesitter"] = require "custom.plugins.nvim-treesitter",
+
+  -- User
   ["goolord/alpha-nvim"] = require "custom.plugins.alpha-nvim",
   ["folke/which-key.nvim"] = require "custom.plugins.which-key",
   ["neovim/nvim-lspconfig"] = require "custom.plugins.nvim-lspconfig",
   ["jose-elias-alvarez/null-ls.nvim"] = require "custom.plugins.null-ls",
   ["Pocco81/TrueZen.nvim"] = require "custom.plugins.trueZen",
   ["Pocco81/auto-save.nvim"] = require "custom.plugins.autosave",
-
-  -- Override
-  ["williamboman/mason.nvim"] = require "custom.plugins.mason",
-  ["kyazdani42/nvim-tree.lua"] = require "custom.plugins.nvim-tree",
-  ["nvim-treesitter/nvim-treesitter"] = require "custom.plugins.nvim-treesitter",
 }
 
 M.mappings = require "custom.mappings"
