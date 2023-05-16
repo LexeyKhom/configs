7c565fc73e2a088496955384cf70fb0040dac8a7
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-05-16 13:08:38 +0300

    feat(chadrc): Base setup

--- chadrc.lua
+++ chadrc.lua
@@ -1,3 +1,21 @@
+local highlights = require "custom.highlights" -- Path to overriding theme and highlights files
+
 local M = {}
+
+M.ui = {
+  theme = "onedark",
+  theme_toggle = { "onedark", "onedark" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+
+  cmp = {
+    style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+  },
+}
+
+M.plugins = "custom.plugins"
+
+M.mappings = require "custom.mappings"
 
 return M
