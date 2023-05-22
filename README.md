87ee3896914036fe1f91ec1b4c5859ddfb941780
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-05-22 11:14:50 +0300

    feat(deno): Add deno support

--- README.md
+++ README.md
@@ -16,4 +16,3 @@
    cd ~/.config/fish;
    git checkout fish;
    ```
-
--- config.fish
+++ config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -g -x fish_greeting ''
+    set -gx fish_greeting ''
+    # Run '/home/lexey/.deno/bin/deno --help' to get started
+    set -gx DENO_INSTALL "/home/lexey/.deno"
+    set -gxa PATH "$DENO_INSTALL/bin"
 end
