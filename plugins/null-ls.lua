53ab6cadb799cea010b6947c0a4148e1f09d07ba
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-23 10:15:12 +0300

    refactor(null-ls): simplify loadSources function

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,3 +1,24 @@
+local loadSources = function()
+  local builtins = require("null-ls").builtins
+  local split = require("custom.utils").split
+  local configs = require("custom.utils").loadLangs "null"
+  local def = {
+    a = "code_actions",
+    c = "completion",
+    d = "diagnostics",
+    f = "formatting",
+  }
+
+  local sources = {}
+  for _, config in pairs(configs) do
+    local shortMode, name = split(config, "[^%.]+") -- split(str, ".") not working
+    local mode = def[shortMode]
+    local source = builtins[mode][name]
+    table.insert(sources, source)
+  end
+  return sources
+end
+
 return {
   "jose-elias-alvarez/null-ls.nvim",
   dependencies = {
@@ -7,27 +28,6 @@ return {
     require("core.utils").load_mappings "null"
   end,
   opts = function()
-    local loadSources = function()
-      local builtins = require("null-ls").builtins
-      local split = require("custom.utils").split
-      local configs = require("custom.utils").loadLangs "null"
-      local def = {
-        a = "code_actions",
-        c = "completion",
-        d = "diagnostics",
-        f = "formatting",
-      }
-
-      local sources = {}
-      for _, config in pairs(configs) do
-        local shortMode, name = split(config, "[^%.]+") -- split(str, ".") not working
-        local mode = def[shortMode]
-        local source = builtins[mode][name]
-        table.insert(sources, source)
-      end
-      return sources
-    end
-
     return {
       debug = true,
       sources = loadSources(),
