d6dfdf559767a59976d7c0b11a8101346b3e8bd8
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-23 09:57:56 +0300

    refactor(highlights): highlights.lua settings in chadrc.lua

--- chadrc.lua
+++ chadrc.lua
@@ -1,13 +1,13 @@
-local highlights = require "custom.highlights" -- Path to overriding theme and highlights files
-
 local M = {}
 
 M.ui = {
   theme = "yoru",
   theme_toggle = { "yoru", "yoru" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+  hl_override = {
+    Visual = { bg = "grey" },
+  },
+  hl_add = {},
 
   cmp = {
     style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
