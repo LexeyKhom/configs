4ee4b8070bfc9f50cf0104673e63d6dd5112016f
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-23 11:57:46 +0300

    feat(langs): Add python support

--- langs.lua
+++ langs.lua
@@ -87,6 +87,24 @@ M.javaScript = {
   },
 }
 
+M.python = {
+  tree = {
+    "python",
+  },
+
+  lsp = {
+    { "pyright", mason = "pyright" },
+    { "ruff_lsp", mason = "ruff-lsp" },
+  },
+
+  null = {
+    { "d.pydocstyle", mason = "pydocstyle" },
+    { "d.mypy", mason = "mypy" },
+    { "d.ruff", mason = "ruff" },
+    { "f.ruff", mason = "ruff" },
+  },
+}
+
 M.cpp = {
   tree = {
     "c",
