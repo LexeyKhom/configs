b99c1f77cef064ddd8109ae264b8f17516fa778e
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-25 09:00:51 +0300

    feat(snippets): Add aiife snippet in javascript

--- init.lua
+++ init.lua
@@ -28,7 +28,7 @@ autocmd("VimResized", {
 local g = vim.g
 
 -- VsCode format for snippets (json)
-vim.g.vscode_snippets_path = "~/.config/nvim/lua/custom/snippets/"
+g.vscode_snippets_path = "~/.config/nvim/lua/custom/snippets/"
 
 -- Default term
 g.term = "float"
