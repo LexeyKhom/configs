63691f0ffc79965a451417e2850cdb5a90ee965b
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-25 16:10:59 +0300

    feat(HUGGING_FACE_HUB_TOKEN): Add token for Hugging Face

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx fish_greeting ''
+    set -gx HUGGING_FACE_HUB_TOKEN (cat ~/GDrive/.hfcc)
+
     # Run '/home/lexey/.deno/bin/deno --help' to get started
     set -gx DENO_INSTALL "/home/lexey/.deno"
     set -gxa PATH "$DENO_INSTALL/bin"
