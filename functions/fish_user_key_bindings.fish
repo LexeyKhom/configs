9e99003253acd52c37c4db7d0ade60466759f7e2
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-27 10:40:45 +0300

    feat(bind): bind [insert mode] Alt+l 'accept-autosuggestion'

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -2,5 +2,5 @@ function fish_user_key_bindings
     # Switch to normal mode (like <ESC>)
     bind -s --preset -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
     bind -s --preset -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
+    bind -M insert \el accept-autosuggestion
 end
-
