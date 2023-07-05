d8ad839702f46d63963d5797d444a1ee2d0f777b
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-07-05 11:58:35 +0300

    fix(termux): chain

--- conf.d/termux.fish
+++ conf.d/termux.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     if test -n "$TERMUX_VERSION"
-        if ! test -d
-            command pkg install proot && termux-chroot
+        if ! test -d /tmp
+            command termux-chroot ||
+                { pkg install proot && termux-chroot }
         end
     end
 end
