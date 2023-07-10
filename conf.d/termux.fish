87dc855dfb2b455befea62d382bbd11c5b18976e
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-07-10 16:04:59 +0300

    refactor(termux): remove some useless code

--- conf.d/termux.fish
+++ conf.d/termux.fish
@@ -1,12 +1,9 @@
 if status is-interactive
     if test -n "$TERMUX_VERSION"
         set -gx isTermux true
-    else
-        set -gx isTermux false
     end
 
     if test "$isTermux" = true
-        echo hello
         if ! test -d /tmp
             if ! command termux-chroot
                 command pkg install proot
