023c16c462d4b7a56c2374e0c3380ce8d2a650a9
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-07-19 14:28:49 +0300

    feat(PATH): append $PATH with ~/.local/bin/

--- config.fish
+++ config.fish
@@ -1,12 +1,13 @@
 # Commands to run in interactive sessions can go here
 if status is-interactive
     set -gx fish_greeting ''
+    set -gxa PATH "$HOME/.local/bin/"
 
     # HuggingFace
     set -gx HUGGING_FACE_HUB_TOKEN (cat ~/GDrive/.hfcc)
 
     # For DENO
     # Run '/home/lexey/.deno/bin/deno --help' to get started
-    set -gx DENO_INSTALL "/home/lexey/.deno"
+    set -gx DENO_INSTALL "$HOME/.deno"
     set -gxa PATH "$DENO_INSTALL/bin"
 end
