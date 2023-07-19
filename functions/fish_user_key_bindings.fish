b7812a83095fb4772163897d63e75abd2e8cfa1a
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-07-19 14:26:15 +0300

    feat(mappings): Add NEW mappings (with alt+l/alt+h)

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,6 +1,29 @@
 function fish_user_key_bindings
     # Switch to normal mode (like <ESC>)
     bind -s --preset -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
-    bind -s --preset -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
-    bind -M insert \el accept-autosuggestion
+
+    # INSERT MODE
+
+    # Ctrl + jklh
+    bind -M insert \ch backward-char
+    bind -M insert \cl forward-char
+
+    # Alt + jklh
+    bind -M insert \ej history-search-forward
+    bind -M insert \ek history-search-backward
+    bind -M insert \eh backward-delete-char
+    bind -M insert \el execute
+
+    # NORMAL MODE
+
+    bind -M default j history-search-forward
+    bind -M default k history-search-backward
+    bind -M default h backward-char
+    bind -M default l forward-char
+
+    # Alt + jklh
+    bind -M default \ej history-search-forward
+    bind -M default \ek history-search-backward
+    bind -M default \eh backward-delete-char
+    bind -M default \el execute
 end
