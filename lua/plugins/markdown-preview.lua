b2499af20df6c566583e447f312731a20a705687
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-12-02 20:41:37 +0300

    feat(markdown-preview): plugin settings

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,10 +1,25 @@
 return {
   "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  cmd = { "MarkdownPreviewToggle" },
   ft = { "markdown" },
   build = "cd app && yarn install",
-  -- FIX: Add keymaps and settings
+  keys = function()
+    require("utils.load").mappings {
+      n = {
+        ["<leader>tm"] = { "<cmd> MarkdownPreviewToggle <CR>", "Toggle MarkdownPreview" },
+      },
+    }
+  end,
   init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
+    local g = vim.g
+    g.mkdp_theme = "dark"
+    g.mkdp_filetypes = { "markdown" }
+    g.mkdp_open_to_the_world = 1
+    g.mkdp_port = "8080"
+
+    g.mkdp_auto_start = 0
+    g.mkdp_auto_close = 0
+    g.mkdp_combine_preview = 1
+    g.mkdp_combine_preview_auto_refresh = 1
   end,
 }
