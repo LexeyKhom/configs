c207a5e795f7716f7df333f68f66949e14bfd87e
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-12-02 19:41:40 +0300

    feat(key_bindings): new mappings with A-f and A-a

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -2,32 +2,34 @@ function fish_user_key_bindings
     # Switch to normal mode (like <ESC>)
     bind -s --preset -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
 
-    # INSERT MODE
+    # Enter / Backspace
+    bind -M default \ef execute
+    bind -M insert \ef execute
+    bind -M default \ea backward-delete-char
+    bind -M insert \ea backward-delete-char
+    bind -M default \es delete-char
+    bind -M insert \es delete-char
 
-    # Ctrl + jklh
-    bind -M insert \ch backward-char
-    bind -M insert \cl forward-char
-
-    # Alt + jklh
+    # Navigation
+    bind -M default \ej history-search-forward
     bind -M insert \ej history-search-forward
-    bind -M insert \ek history-search-backward
-    bind -M insert \eh backward-delete-char
-    bind -M insert \el execute
-
-    bind -M insert \ed exit
-
-    # NORMAL MODE
-
     bind -M default j history-search-forward
+
+    bind -M default \ek history-search-backward
+    bind -M insert \ek history-search-backward
     bind -M default k history-search-backward
+
+    bind -M default \eh backward-char
+    bind -M insert \eh backward-char
     bind -M default h backward-char
+
+    bind -M default \el forward-char
+    bind -M insert \el forward-char
     bind -M default l forward-char
 
-    # Alt + jklh
-    bind -M default \ej history-search-forward
-    bind -M default \ek history-search-backward
-    bind -M default \eh backward-delete-char
-    bind -M default \el execute
-
+    # Other
     bind -M default \ed exit
+    bind -M insert \ed exit
+    bind -M default \ec cancel-commandline
+    bind -M insert \ec cancel-commandline
 end
