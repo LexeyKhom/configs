2c854de401fb740eba95d66116e6aa88d96a365b
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-12-30 23:01:23 +0300

    feat(noice): add highlights and some settings for noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,24 +2,41 @@ return {
   "folke/noice.nvim",
   dependencies = {
     "MunifTanjim/nui.nvim",
+    "LexeyKhom/nvconfig",
   },
   event = "VeryLazy",
-  opts = {
-    lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+  opts = function()
+    local isTermux = vim.g.isTermux
+    return {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
       },
-    },
-    presets = {
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      lsp_doc_border = true, -- add a border to hover docs and signature help
-      inc_rename = true, -- enables an input dialog for inc-rename.nvim
-    },
-  },
+      presets = {
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+      },
+      views = {
+        cmdline_popup = {
+          position = { row = 5, col = "50%" },
+          size = { width = isTermux and 36 or 60, height = "auto" },
+          border = { style = "none", padding = { 1, 2 } },
+          win_options = { winhighlight = { Normal = "NoiceLightFloatNormal", FloatBorder = "NoiceLightFloatBorder" } },
+        },
+        popupmenu = {
+          position = { row = 8, col = "50%" },
+          size = { width = isTermux and 36 or 60, height = 10 },
+          border = { style = "none", padding = { 1, 2 } },
+          win_options = { winhighlight = { Normal = "NoiceDarkFloatNormal", FloatBorder = "NoiceDarkFloatBorder" } },
+        },
+      },
+    }
+  end,
   config = function(_, opts)
     require("noice").setup(opts)
   end,
---
+++ lua/plugins/nvconfig/hl_add/noice.lua
@@ -0,0 +1,6 @@
+return {
+  NoiceLightFloatNormal = { bg = "black2" },
+  NoiceLightFloatBorder = { bg = "black2" },
+  NoiceDarkFloatNormal = { bg = "darker_black" },
+  NoiceDarkFloatBorder = { bg = "darker_black" },
+}
