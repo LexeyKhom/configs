02420e71622a02084cd4692de5bbd41f886df81f
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2024-01-16 20:10:28 +0300

    fix(indent-blankline): fix "always red blankline"

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -22,5 +22,7 @@ return {
   end,
   config = function(_, opts)
     require("ibl").setup(opts)
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end,
 }
