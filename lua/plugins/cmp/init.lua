02fe959a04b00cbc61f25d7daa028780a2959673
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2024-01-22 17:08:28 +0300

    feat(cmp): add jump mapping on <Tab> and <S-Tab>

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -30,6 +30,18 @@ return {
   opts = function()
     local KEYS = require "core.mappings"
     local cmp = require "cmp"
+    local luasnip = require "luasnip"
+
+    local jump = function(direction)
+      return function(fallback)
+        if luasnip.jumpable(direction) then
+          luasnip.jump(direction)
+        else
+          fallback()
+        end
+      end
+    end
+
     return {
       completion = {
         completeopt = "menu,menuone",
@@ -50,23 +62,23 @@ return {
         format = require "plugins.cmp.formatter",
       },
       mapping = {
-        [KEYS.ENTER] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Insert,
-          select = true,
-        },
-        [KEYS.UP] = cmp.mapping.select_prev_item(),
+        [KEYS.ENTER] = cmp.mapping.confirm { select = true },
+        ["<Enter>"] = cmp.mapping.confirm { select = true },
+
         [KEYS.DOWN] = cmp.mapping.select_next_item(),
+        [KEYS.UP] = cmp.mapping.select_prev_item(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+
+        ["<Tab>"] = cmp.mapping(jump(1), { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(jump(-1), { "i", "s" }),
+
         [KEYS.SCROLL_DOWN] = cmp.mapping.scroll_docs(4),
         [KEYS.SCROLL_UP] = cmp.mapping.scroll_docs(-4),
 
-        ["<Enter>"] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Insert,
-          select = true,
-        },
-        ["<Up>"] = cmp.mapping.select_prev_item(),
-        ["<Down>"] = cmp.mapping.select_next_item(),
         ["<A-w>"] = cmp.mapping.complete(),
         [KEYS.CLOSE] = cmp.mapping.close(),
+        [KEYS.QUIT] = cmp.mapping.close(),
         [KEYS.CANCEL] = cmp.mapping.abort(),
       },
       sources = {
@@ -79,7 +91,7 @@ return {
       },
       snippet = {
         expand = function(args)
-          require("luasnip").lsp_expand(args.body)
+          luasnip.lsp_expand(args.body)
         end,
       },
     }
