c3b2247e36b758a98eeeb57e4c5a77784417f9f6
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2024-02-02 18:31:31 +0300

    feat(mappings): add polifils for <A-w,e,b,0,4> mappings in insert mode

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -2,6 +2,11 @@ function fish_user_key_bindings
     # Switch to normal mode (like <ESC>)
     bind -s --preset -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
 
+    # Paste
+    bind -M default \ep fish_clipboard_paste
+    bind -M insert \ep fish_clipboard_paste
+    bind -M default p fish_clipboard_paste
+
     # Enter / Backspace
     bind -M default \ef execute
     bind -M insert \ef execute
@@ -19,13 +24,31 @@ function fish_user_key_bindings
     bind -M insert \ek history-search-backward
     bind -M default k history-search-backward
 
+    bind -M default \el forward-char
+    bind -M insert \el forward-char
+    bind -M default l forward-char
+
     bind -M default \eh backward-char
     bind -M insert \eh backward-char
     bind -M default h backward-char
 
-    bind -M default \el forward-char
-    bind -M insert \el forward-char
-    bind -M default l forward-char
+    bind -M default \ew forward-word forward-char
+    bind -M insert \ew forward-word forward-char
+    bind -M default w forward-word forward-char
+
+    bind -M default \ee forward-word
+    bind -M insert \ee forward-word
+    bind -M default e forward-word
+
+    bind -M default \eb backward-word
+    bind -M insert \eb backward-word
+    bind -M default b backward-word
+
+    bind -M default \e0 beginning-of-line
+    bind -M insert \e0 beginning-of-line
+
+    bind -M default \e4 end-of-line
+    bind -M insert \e4 end-of-line
 
     # Other
     bind -M default \ed exit
