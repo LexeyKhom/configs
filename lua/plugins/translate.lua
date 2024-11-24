774d5df3bde2a5b64fff08f8746f8add58eda22a
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2024-11-24 23:47:57 +0300

    chore(plugins/translate): update config

--- lua/plugins/translate.lua
+++ lua/plugins/translate.lua
@@ -1,163 +1,57 @@
+local function translate(to_lang, input_type)
+  return function()
+    require("translate").translate {
+      get_command = function(input)
+        return {
+          "trans",
+          "-e",
+          "google",
+          "-b",
+          ":" .. to_lang,
+          input,
+        }
+      end,
+      -- input | clipboard | selection
+      input = input_type,
+      -- open_float | notify | copy | insert | replace
+      output = { "open_float", "copy" },
+      resolve_result = function(result)
+        if result.code ~= 0 then
+          return nil
+        end
+        return string.match(result.stdout, "(.*)\n")
+      end,
+    }
+  end
+end
+
 return {
   "niuiic/translate.nvim",
-  cmd = {
-    "TransToRU",
-    "TransToEN",
-    "TransToENfromInput",
-    "TransToRUfromInput",
-  },
-  dependencies = { "niuiic/core.nvim" },
+  dependencies = { "niuiic/omega.nvim" },
   keys = function()
     require("utils.load").mappings {
       n = {
         ["<leader>tr"] = {
-          "<cmd> TransToRUfromInput <CR>",
+          translate("ru", "input"),
           "Translate to RU",
         },
         ["<leader>te"] = {
-          "<cmd> TransToENfromInput <CR>",
+          translate("en", "input"),
           "Translate to EN",
         },
       },
 
       v = {
         ["<leader>tr"] = {
-          "<cmd> TransToRU <CR>",
+          translate("ru", "selection"),
           "Translate to RU",
         },
 
         ["<leader>te"] = {
-          "<cmd> TransToEN <CR>",
+          translate("en", "selection"),
           "Translate to EN",
         },
       },
     }
-  end,
-  opts = {
-    output = {
-      float = {
-        -- max_width of float window
-        max_width = 60,
-        -- max_height of float window
-        max_height = 8,
-        -- whether close float window on cursor move
-        close_on_cursor_move = true,
-        -- key to enter float window
-        enter_key = "T",
-      },
-    },
-    translate = {
-      {
-        -- use :TransToRU to start this job
-        cmd = "TransToRU",
-        -- shell command
-        -- translate-shell is used here
-        command = "trans",
-        -- shell command args
-        args = function(trans_source)
-          -- trans_source is the text you want to translate
-          return {
-            "-b",
-            "-e",
-            "google",
-            -- use proxy
-            -- "-x",
-            -- "http://127.0.0.1:10025",
-            "-t",
-            "ru",
-            -- you can filter translate source here
-            trans_source,
-          }
-        end,
-        -- how to get translate source
-        -- selection | input | clipboard
-        input = "selection",
-        -- how to output translate result
-        -- float_win | notify | clipboard | insert
-        output = { "float_win" },
-        -- format output
-        ---@type fun(output: string): string
-        format = function(output)
-          return output
-        end,
-      },
-      {
-        -- use :TransToEN to start this job
-        cmd = "TransToEN",
-        -- shell command
-        -- translate-shell is used here
-        command = "trans",
-        -- shell command args
-        args = function(trans_source)
-          -- trans_source is the text you want to translate
-          return {
-            "-b",
-            "-e",
-            "google",
-            -- use proxy
-            -- "-x",
-            -- "http://127.0.0.1:10025",
-            "-t",
-            "en",
-            -- you can filter translate source here
-            trans_source,
-          }
-        end,
-        -- how to get translate source
-        -- selection | input | clipboard
-        input = "selection",
-        -- how to output translate result
-        -- float_win | notify | clipboard | insert
-        output = { "float_win" },
-        -- format output
-        ---@type fun(output: string): string
-        format = function(output)
-          return output
-        end,
-      },
-      {
-        cmd = "TransToRUfromInput",
-        command = "trans",
-        args = function(trans_source)
-          return {
-            "-b",
-            "-e",
-            "google",
-            "-t",
-            "ru",
-            trans_source,
-          }
-        end,
-        input = "input",
-        output = { "float_win" },
-        ---@type fun(output: string): string
-        format = function(output)
-          return output
-        end,
-      },
-      {
-        cmd = "TransToENfromInput",
-        command = "trans",
-        args = function(trans_source)
-          return {
-            "-b",
-            "-e",
-            "google",
-            "-t",
-            "en",
-            trans_source,
-          }
-        end,
-        input = "input",
-        output = { "float_win" },
-        ---@type fun(output: string): string
-        format = function(output)
-          return output
-        end,
-      },
-    },
-  },
-  config = function(_, opts)
-    require("translate").setup(opts)
   end,
 }
