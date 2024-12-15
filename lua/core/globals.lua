43db892825fbc2124453618b871d82e36d2f3153
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2024-12-15 17:00:47 +0300

    feat(String Interpolation): example ("${name}" % { name="foo" }) -> "foo"

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -10,3 +10,14 @@ local providers = { "node", "perl", "python3", "ruby" }
 for _, provider in ipairs(providers) do
   g["loaded_" .. provider .. "_provider"] = 0
 end
+
+-- String Interpolation
+-- Input ("${name} is ${value}" % { name = "foo", value = "bar" })
+-- Outputs "foo is bar"
+getmetatable("").__mod = function(s, tab)
+  return (
+    s:gsub("($%b{})", function(w)
+      return tab[w:sub(3, -2)] or w
+    end)
+  )
+end
