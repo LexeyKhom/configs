d7f5143d5a78e49e8c9b81371dd1dbad9416d30c
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2024-12-26 16:33:31 +0300

    chore(colorizer): update colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,33 +1,45 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  event = { "BufRead", "BufNewFile" },
+  "catgoose/nvim-colorizer.lua",
+  event = { "BufReadPre" },
   opts = function()
     return {
       filetypes = { "*" },
       user_default_options = {
-        RGB = true,          -- #RGB hex codes
-        RRGGBB = true,       -- #RRGGBB hex codes
-        names = true,        -- "Name" codes like Blue or blue
-        RRGGBBAA = true,     -- #RRGGBBAA hex codes
-        AARRGGBB = true,     -- 0xAARRGGBB hex codes
-        rgb_fn = true,       -- CSS rgb() and rgba() functions
-        hsl_fn = true,       -- CSS hsl() and hsla() functions
-        css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true,       -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = "background", -- Set the display mode.
-        -- Available methods are false / true / "normal" / "lsp" / "both"
-        -- True is same as normal
-        tailwind = true,                               -- Enable tailwind colors
+        names = true, -- "Name" codes like Blue or blue
+        -- Expects a table of color name to rgb value pairs.  # is optional
+        -- Example: { cool = "#107dac", ["notcool"] = "ee9240" }
+        -- Set to false|nil to disable
+        names_custom = {
+          darkpurple = "#592147",
+        }, -- Custom names to be highlighted: table|function|false|nil
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Highlighting mode.  'background'|'foreground'|'virtualtext'
+        mode = "background", -- Set the display mode
+        -- Tailwind colors.  boolean|'normal'|'lsp'|'both'.  True is same as normal
+        tailwind = true, -- Enable tailwind colors
         -- parsers can contain values used in |user_default_options|
         sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+        -- Virtualtext character to use
         virtualtext = "■",
+        -- Display virtualtext inline with color
+        virtualtext_inline = false,
+        -- Virtualtext highlight mode: 'background'|'foreground'
+        virtualtext_mode = "foreground",
         -- update color values even if buffer is not focused
         -- example use: cmp_menu, cmp_docs
         always_update = false,
       },
       -- all the sub-options of filetypes apply to buftypes
       buftypes = {},
+      -- Boolean | List of usercommands to enable
+      user_commands = true, -- Enable all or some usercommands
     }
   end,
   config = function(_, opts)
