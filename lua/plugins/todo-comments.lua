9d58ba5eb30483b8f8ef9c822a67e1ac79b76df8
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2024-12-26 16:34:52 +0300

    chore(todo-comments): update icons

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -5,21 +5,13 @@ return {
   event = { "BufRead", "BufNewFile" },
   opts = {
     keywords = {
-      FIX = {
-        icon = " ",
-        color = "error",
-        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
-      },
-      TODO = { icon = " ", color = "info" },
-      HACK = { icon = " ", color = "warning" },
-      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-      TEST = {
-        icon = "⏲ ",
-        color = "test",
-        alt = { "TESTING", "PASSED", "FAILED" },
-      },
+      FIX = { icon = " ", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+      TODO = { icon = " " },
+      HACK = { icon = " " },
+      WARN = { icon = " ", alt = { "WARNING", "XXX" } },
+      PERF = { icon = "󱎫 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = "󰎚 ", alt = { "INFO" } },
+      TEST = { icon = "󰙨 ", alt = { "TESTING", "PASSED", "FAILED" } },
     },
   },
   keys = function()
