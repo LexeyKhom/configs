b573c3f14fbf5d145d91244dad7061e81e016f82
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-01-01 22:33:58 +0300

    chore(which-key): remove useless code

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,8 +1,4 @@
 return {
   "folke/which-key.nvim",
-  cmd = "WhichKey",
-  keys = { "<leader>", "c", "d", "y", "v", "g", "[", "]", '"', "'", "`" },
-  config = function(_, opts)
-    require("which-key").setup(opts)
-  end,
+  event = "VeryLazy",
 }
