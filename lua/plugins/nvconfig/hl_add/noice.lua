2a3bc899be2b3a15acf127bb7e09ced38b2bc70e
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-01-02 18:38:52 +0300

    chore(hl_add/noice): add NoiceCmdlinePopupBorderInput

--- lua/plugins/nvconfig/hl_add/noice.lua
+++ lua/plugins/nvconfig/hl_add/noice.lua
@@ -3,4 +3,5 @@ return {
   NoiceLightFloatBorder = { bg = "black2" },
   NoiceDarkFloatNormal = { bg = "darker_black" },
   NoiceDarkFloatBorder = { bg = "darker_black" },
+  NoiceCmdlinePopupBorderInput = { bg = "black2", fg = "black2" },
 }
