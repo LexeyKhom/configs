a8e3d32bb3011fcb8adc201fd04bfdd28886d9b1
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-01-02 18:38:17 +0300

    refactor(lspconfig): refactor lspconfig/init

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -16,24 +16,27 @@ return {
   end,
   opts = function()
     local opts = {}
+
+    local servers, settings = require("utils.load").langs "lsp"
+    opts.servers = servers
+    opts.settings = settings
+
     opts.on_attach = function(_, bufnr)
       local maps = require "plugins.lspconfig.mappings"
       require("utils.load").mappings(maps, { buffer = bufnr })
     end
-    -- opts.capabilities = require("cmp_nvim_lsp").default_capabilities()
-    opts.capabilities = vim.lsp.protocol.make_client_capabilities()
-    opts.capabilities.textDocument.completion =
-        require("cmp_nvim_lsp").default_capabilities().textDocument.completion
+
+    opts.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
     return opts
   end,
   config = function(_, opts)
     local lspconfig = require "lspconfig"
-    local servers, settings = require("utils.load").langs "lsp"
-    for _, lsp in ipairs(servers) do
+    for _, lsp in ipairs(opts.servers) do
       lspconfig[lsp].setup {
         on_attach = opts.on_attach,
         capabilities = opts.capabilities,
-        settings = settings[lsp] or {},
+        settings = opts.settings[lsp] or {},
       }
     end
   end,
