3fccacd634b440d6b5bc7b2d185a4b3284fd6a05
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-01-02 18:40:20 +0300

    chore(sql-ui): remove useless "vim-dadbod-completion" dependencies

--- lua/plugins/sql-ui.lua
+++ lua/plugins/sql-ui.lua
@@ -11,11 +11,6 @@ return {
   "kristijanhusak/vim-dadbod-ui",
   dependencies = {
     { "tpope/vim-dadbod", lazy = true },
-    {
-      "kristijanhusak/vim-dadbod-completion",
-      ft = { "sql", "mysql", "plsql" },
-      lazy = true,
-    },
   },
   keys = function()
     require("utils.load").mappings {
