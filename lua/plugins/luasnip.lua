d52ed3068f397fedfab433b60c136fcd239235f3
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-08-01 15:24:00 +0300

    fix(luasnip): return unlink the current snippet on InsertLeave

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -7,5 +7,17 @@ return {
     local snippets_path = vim.fn.stdpath "config" .. "/.snippets"
     require("luasnip.loaders.from_vscode").lazy_load { exclude = { "gdscript" } }
     require("luasnip.loaders.from_vscode").lazy_load { paths = snippets_path }
+
+    -- Unlink the current LuaSnip snippet on InsertLeave if no jump is active.
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      callback = function()
+        if
+          require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+          and not require("luasnip").session.jump_active
+        then
+          require("luasnip").unlink_current()
+        end
+      end,
+    })
   end,
 }
