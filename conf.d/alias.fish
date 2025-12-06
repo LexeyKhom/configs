778e1fc36c9a16c05bac87f3c1ccc80720ecf61f
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-12-06 12:46:29 +0300

    feat(alias): add rclone-upload and rclone-download aliases

--- conf.d/alias.fish
+++ conf.d/alias.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     # For NoeVim tests
     alias tvim='XDG_DATA_HOME=~/Documents/NeoVim/.local/share XDG_CONFIG_HOME=~/Documents/NeoVim/ nvim'
+
+    # Rclone sync
+    alias 'rclone-upload'='echo "Upload from $DRIVE to $REMOTE_DRIVE"; rclone sync $DRIVE $REMOTE_DRIVE  --exclude-from $RCLONE_IGNORE --delete-excluded -Pi'
+    alias 'rclone-download'='echo "Download from $REMOTE_DRIVE to $DRIVE"; rclone sync $REMOTE_DRIVE $DRIVE --exclude-from $RCLONE_IGNORE -Pi'
 end
