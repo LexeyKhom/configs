25ec424671b04ca404d056e2b41533ef922a70ad
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2025-12-06 17:22:09 +0300

    feat(langs): add make support

---
+++ lua/langs/make.lua
@@ -0,0 +1,13 @@
+return {
+  tree = {
+    "make",
+  },
+
+  lsp = {
+    { "autotools_ls", mason = "autotools-language-server" },
+  },
+
+  null = {
+    { "d.checkmake", mason = "checkmake" },
+  },
+}
