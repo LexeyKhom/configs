74e5d32b82f3642b4ce8a0560255cbf436b049bd
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2026-02-24 18:48:18 +0300

    feat(godot): add gdshader support

--- lua/langs/godot.lua
+++ lua/langs/godot.lua
@@ -1,11 +1,17 @@
 return {
   tree = {
     "gdscript",
+    "gdshader",
     "godot_resource",
   },
 
   lsp = {
-    { "gdscript", filter = { "mason" } },
+    {
+      "gdscript",
+      filter = { "mason" },
+      setting = { extra_filetypes = { "gdshader" } },
+    },
+    { "gdshader_lsp", filter = { "mason" } },
   },
 
   null = {
