a8c37af38f5b4d2307451b2dad1caf0787f977cd
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2026-07-29 12:52:29 +0300

    BREAKING CHANGE: remove treesitter, add tree-sitter-manager

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,16 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
+  "romus204/tree-sitter-manager.nvim",
   dependencies = {
     "RRethy/nvim-treesitter-endwise",
     { "windwp/nvim-ts-autotag", opts = {} },
   },
-  event = { "BufReadPost", "InsertEnter" },
-  cmd = {
-    "TSUpdate",
-    "TSInstall",
-    "TSBufEnable",
-    "TSBufDisable",
-    "TSModuleInfo",
-  },
-  build = ":TSUpdate",
   opts = function()
     return {
       ensure_installed = require("utils.loader").langs "tree",
       auto_install = true,
-      indent = { enable = true },
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-      },
-
-      -- Plugins settings
-      endwise = {
-        enable = true,
-      },
     }
   end,
   config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    require("tree-sitter-manager").setup(opts)
   end,
 }
